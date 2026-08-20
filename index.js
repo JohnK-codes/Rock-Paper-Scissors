01d261bfe0fca5dc790148a6c527650ec8d959f7
John <gkriarakis05@gmail.com>
2026-08-20 14:17:18 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-function getComputerChoice(Rock, Paper, Scissors) {
-  return (Rock, Paper, Scissors);
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  return prompt("Choose between rock, paper and scissors");
 }
 
-let result = getComputerChoice("Rock", "Paper", "Scissors");
-console.log(result);
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toUpperCase();
+  console.log(humanChoice);
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+function playGame(playRound) {
+  let score = 0;
+  if (humanSelection !== computerSelection) {
+    return "Congratulations you won!" && score + 1;
+  } else {
+    return "loser";
+  }
+}
+
+playGame();
+playGame();
+playGame();
+playGame();
