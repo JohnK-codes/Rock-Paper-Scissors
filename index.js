8bee88edff98be406d7500c3e32918859894af03
JohnK-Codes <gkriarakis05@gmail.com>
2026-08-21 13:23:54 +0300

    Fix JavaScript

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
-  return choices[Math.floor(Math.random() * choices.length)];
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+return choices[Math.floor(Math.random() * choices.length)];
 }
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-  return prompt("Choose between rock, paper and scissors");
+return prompt("Choose between rock, paper and scissors");
 }
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toUpperCase();
-  console.log(humanChoice);
+humanChoice = humanChoice.toUpperCase();
+console.log(humanChoice);
+if (humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+return "You won";
+} else if (humanChoice ==="PAPER" && computerChoice === "ROCK"){
+return "You won";
+} else if (humanChoice === "SCISSORS" && computerChoice === "PAPER"){
+return "You won"
+} else if (humanChoice === computerChoice){
+return "Draw";
+}else{
+return "Loser";
+}
+
 }
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+alert(computerSelection)
+alert(playRound(humanSelection, computerSelection));
 
-function playGame(playRound) {
-  let score = 0;
-  if (humanSelection !== computerSelection) {
-    return "Congratulations you won!" && score + 1;
-  } else {
-    return "loser";
-  }
-}
 
-playGame();
-playGame();
-playGame();
-playGame();
+
+
