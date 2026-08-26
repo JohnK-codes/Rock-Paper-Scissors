41a71f481a32cc2614200a15b3e0b63117d49702
John <gkriarakis05@gmail.com>
2026-08-26 22:34:16 +0300

    Fix game

--- index.js
+++ index.js
@@ -8,22 +8,40 @@ function getHumanChoice() {
   return prompt("Choose between rock, paper and scissors");
 }
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toUpperCase();
-  console.log(humanChoice);
-  if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
-    return "You won";
-  } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
-    return "You won";
-  } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
-    return "You won";
-  } else if (humanChoice === computerChoice) {
-    return "Draw";
-  } else {
-    return "Loser";
+function playGame() {
+  let computerScore = 0;
+  let humanScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    if (humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+      return "You won";
+    } else if (humanChoice === "PAPER" && computerChoice === "ROCK") {
+      return "You won";
+    } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+      return "You won";
+    } else if (humanChoice === computerChoice) {
+      return "Draw";
+    } else {
+      return "Loser";
+    }
+  }
+
+  let round = 1;
+  for (; round < 6; round++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    alert(playRound(humanSelection, computerSelection));
+    const result = playRound(humanSelection, computerSelection);
+    if (result === "You won") {
+      humanScore++;
+    } else if (playRound(humanSelection, computerSelection) === "Loser") {
+      computerScore++;
+    } else if (playRound(humanSelection, computerSelection) === "Draw") {
+      humanScore++;
+      computerScore++;
+      const result = playRound(humanSelection, computerSelection);
+    }
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-alert(playRound(humanSelection, computerSelection));
+playGame();
